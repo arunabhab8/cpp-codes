4e410f2c84024aa72e198c6602df6b53b15964c4
Arunabha Bhaumik <arunabha@Arunabhas-MacBook-Air.local>
2026-09-12 09:03:05 +0530

    commit changes

---
+++ Practice389.cpp
@@ -0,0 +1,24 @@
+// Data Type Modifiers
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(long long) << endl;
+
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof (long double) << endl;
+
+    cout << sizeof(short int) << endl;
+
+    cout << sizeof(signed int) << endl;
+    cout << sizeof(unsigned int) << endl;
+
+    return 0;
+}
